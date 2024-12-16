ed96ef9a20b41214731eecd96a092bbda4a418cf
PSkinnerTech <patrick@clgcorporation.com>
2024-12-15 22:57:49 -0500

    remove unnecessary folders

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node --no-deprecation
-export declare function getTagValue(list: {
-    name: string;
-    value: string;
-}[], name: string): string | undefined;
--- dist/turbo/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { DeployArgs } from '../types.js';
-export default function TurboDeploy(argv: DeployArgs, jwk: any): Promise<string | null>;
