b6fe77807a7f645f6442e42e6f40d62520f296d1
PSkinnerTech <patrick@clgcorporation.com>
2024-12-28 08:51:09 -0500

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,41 +7,42 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended'
 	],
 	rules: {
-		// Allow console statements in our CLI tool
+		// Disable console warnings for CLI tool
 		'no-console': 'off',
 		
-		// Enforce return types except for test files
-		'@typescript-eslint/explicit-function-return-type': ['error', {
-			allowExpressions: true,
-			allowTypedFunctionExpressions: true,
-			allowHigherOrderFunctions: true,
-			allowDirectConstAssertionInArrowFunctions: true,
-			allowConciseArrowFunctionExpressionsStartingWithVoid: true,
-			allowedNames: ['test', 'it', 'describe', 'beforeEach', 'afterEach', 'beforeAll', 'afterAll']
-		}],
-
-		// Be explicit about unused vars
+		// Handle unused vars
 		'@typescript-eslint/no-unused-vars': ['error', {
-			argsIgnorePattern: '^_',
-			varsIgnorePattern: '^_',
-			caughtErrorsIgnorePattern: '^_'
+			'argsIgnorePattern': '^_',
+			'varsIgnorePattern': '^_'
 		}],
 
 		// Allow any in specific cases
 		'@typescript-eslint/no-explicit-any': ['error', {
-			ignoreRestArgs: true,
-			fixToUnknown: false
+			'ignoreRestArgs': true
+		}],
+
+		// Return types
+		'@typescript-eslint/explicit-function-return-type': ['error', {
+			'allowExpressions': true,
+			'allowTypedFunctionExpressions': true,
+			'allowHigherOrderFunctions': true,
+			'allowDirectConstAssertionInArrowFunctions': true,
+			'allowConciseArrowFunctionExpressionsStartingWithVoid': true
 		}]
 	},
 	overrides: [
 		{
 			// Relax rules for test files
-			files: ['**/*.test.ts'],
+			files: ['**/*.test.ts', '**/*.spec.ts'],
 			rules: {
 				'@typescript-eslint/no-explicit-any': 'off',
 				'@typescript-eslint/no-unused-vars': 'off',
 				'@typescript-eslint/explicit-function-return-type': 'off'
 			}
 		}
-	]
+	],
+	env: {
+		node: true,
+		es2021: true
+	}
 };
